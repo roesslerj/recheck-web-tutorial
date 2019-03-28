cf7d0d5c8788725ae721f0e0574bac9e52a3a720
roesslerj <jeremias.roessler@retest.de>
2019-03-28 14:55:18 +0100

    Adding first test for Google

---
+++ src/test/java/com/mycompany/MyFirstTest.java
@@ -0,0 +1,38 @@
+package com.mycompany;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import de.retest.recheck.Recheck;
+import de.retest.recheck.RecheckImpl;
+
+public class MyFirstTest {
+
+	private WebDriver driver;
+	private Recheck re;
+
+	@Before
+	public void setup() {
+		re = new RecheckImpl();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\retest\\Desktop\\recheck-web-tutorial\\src\\test\\resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void google() throws Exception {
+		re.startTest();
+		driver.get("http://google.com");
+		re.check(driver, "open");
+
+		re.capTest();
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+		re.cap();
+	}
+}
