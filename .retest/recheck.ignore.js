f27df75d4d029009c17645471e6b4cfc05520a62
roesslerj <jeremias.roessler@retest.de>
2019-03-28 14:57:03 +0100

    Adding generated retest files and adapted recheck.ignore

---
+++ .retest/recheck.ignore.js
@@ -0,0 +1,9 @@
+// Here you can implement ignore rules for recheck in javascript.
+// Please do not delete this file, even if it is empty.
+
+// You can implement either of these two functions:
+// shouldIgnoreElement(element)
+// shouldIgnoreAttributeDifference(element, diff)
+
+// You can find more details and example rules at: 
+// https://retest.github.io/docs/recheck/how-ignore-works-in-recheck/
