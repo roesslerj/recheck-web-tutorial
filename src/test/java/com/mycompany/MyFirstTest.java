d25fe796efab1ee080c3fe1e70e3de12a916208b
Jeremias Roessler <roessler@retest.de>
2019-04-25 00:32:50 +0200

    Making Chrome execute headless

--- src/test/java/com/mycompany/MyFirstTest.java
+++ src/test/java/com/mycompany/MyFirstTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import de.retest.recheck.Recheck;
 import de.retest.recheck.RecheckImpl;
@@ -17,7 +18,16 @@ public class MyFirstTest {
 	@Before
 	public void setup() {
 		re = new RecheckImpl();
-		driver = new ChromeDriver();
+
+		final ChromeOptions opts = new ChromeOptions();
+		opts.addArguments(
+				// Enable headless mode for faster execution.
+				"--headless",
+				// Use Chrome in container-based Travis CI environment (see https://docs.travis-ci.com/user/chrome#Sandboxing).
+				"--no-sandbox",
+				// Fix window size for stable results.
+				"--window-size=1200,800" );
+		driver = new ChromeDriver(opts);
 	}
 
 	@Test
